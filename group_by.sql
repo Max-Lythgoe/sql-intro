85bc0254aad51c73ba2809fef7487d356451691e
Max Lythgoe <lythgoemax@gmail.com>
2022-03-28 14:37:27 -0600

    Finished group_by

---
+++ group_by.sql
@@ -0,0 +1,14 @@
+-- SELECT SUM(total)
+-- FROM invoice
+-- GROUP BY billing_state;
+
+-- SELECT AVG(milliseconds), album_id
+-- FROM track
+-- GROUP BY album_id
+-- ORDER BY AVG
+
+-- SELECT artist_id, COUNT(album_id)
+-- FROM album
+-- WHERE artist_id IN (8, 22)
+-- GROUP BY artist_id
+
